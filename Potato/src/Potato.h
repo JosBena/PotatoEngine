c52de1cc0085390dd4b4afaf23b0bb0c350769b7
JosBena <162043587+JosBena@users.noreply.github.com>
2024-04-17 23:12:46 -0400

    set up premake and Log

--- Potato/src/Potato.h
+++ Potato/src/Potato.h
@@ -3,6 +3,7 @@
 // For use by Poato applicaitons
 
 #include "Potato/Application.h"
+#include "Potato/Log.h"
 
 //--Entry Point -------
 #include "Potato/EntryPoint.h"
---
+++ Potato/src/Potato/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+
+namespace Potato {
+	class POTATO_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger;  }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger;  }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+//Core log macros
+#define PT_CORE_TRACE(...)   ::Potato::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define PT_CORE_INFO(...)    ::Potato::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define PT_CORE_WARN(...)    ::Potato::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define PT_CORE_ERROR(...)   ::Potato::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define PT_CORE_FATAL(...)   ::Potato::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+//client log macros
+#define PT_TRACE(...)   ::Potato::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define PT_INFO(...)    ::Potato::Log::GetClientLogger()->info(__VA_ARGS__)
+#define PT_WARN(...)    ::Potato::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define PT_ERROR(...)   ::Potato::Log::GetClientLogger()->error(__VA_ARGS__)
+#define PT_FATAL(...)   ::Potato::Log::GetClientLogger()->fatal(__VA_ARGS__)
